e3842a0d7b8b75b5d52088bb9c33ca885075b63d
liyongjun <liyongjun@crf-tech.com>
2021-07-28 22:43:56 +0800

    自定义过滤器  base  index

--- 自定义过滤器.py
+++ 自定义过滤器.py
@@ -7,11 +7,18 @@ app.config.from_object(settings)
 
 
 # 使用add_template_filter
+# @app.route('/')
+# def hello_everyone():
+#     msg = 'hi hello hi everyone'
+#     list1 = [1, 3, 5, 9]
+#     return render_template('自定义过滤器.html', msg=msg, list1=list1)
+
+
 @app.route('/')
 def hello_everyone():
-    msg = 'hello hi everyone'
+    msg = 'hi hello hi everyone'
     list1 = [1, 3, 5, 9]
-    return render_template('自定义过滤器.html', msg=msg, list1=list1)
+    return render_template('base.html', msg=msg, list1=list1)
 
 
 def replace_hello(value):
