d2f9c5213590ac223ec6ff68402312a172da5b19
liyongjun <1477676296@qq.com>
2021-07-29 23:47:54 +0800

    蓝图

--- apps/__init__.py
+++ apps/__init__.py
@@ -4,7 +4,7 @@ from apps.user.view import user_bp
 
 
 def create_app():
-    app = Flask(__name__)  # app是一个核心对象
+    app = Flask(__name__,template_folder='../templates/',static_folder='../static/')  # app是一个核心对象
     app.config.from_object(settings)  # 加载配置
     # 蓝图
     app.register_blueprint(user_bp)  # 将蓝图对象绑定到app上
--- apps/user/view.py
+++ apps/user/view.py
@@ -1,21 +1,50 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect
+
+from apps.user.model import User
 
 user_bp = Blueprint('user', __name__)
+
+users = list()
 
 
 @user_bp.route('/')
 def user_center():
-    return '用户中心'
+    return render_template('user/show.html', users=users)
 
 
 @user_bp.route('/register', methods=['GET', 'POST'])
 def register():
-    return '用户注册'
+    if request.method == 'POST':
+        # 获取post提交的数据
+        username = request.form.get('username')
+        password = request.form.get('password')
+        repassword = request.form.get('repassword')
+        phone = request.form.get('phone')
+        if password == repassword:
+            for user in users:
+                if user.username == username:
+                    return render_template('user/register.html', msg='用户名已存在')
+            user = User(username, password, repassword)
+            users.append(user)
+            return redirect('/')
+
+    return render_template('user/register.html')
 
 
 @user_bp.route('/login', methods=['GET', 'POST'])
 def login():
     return '用户登录'
+
+
+@user_bp.route('/del')
+def del_user():
+    username = request.args.get('username')
+    for user in users:
+        if user.username == username:
+            users.remove(user)
+            return redirect('/')
+    else:
+        return '删除失败'
 
 
 @user_bp.route('/logout', methods=['GET', 'POST'])
